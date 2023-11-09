48189f31d9f360132e55c92559154240fe98eced
Peti1991 <fodor.peter.1991@gmail.com>
2023-11-09 01:02:29 +0100

    finalized

---
+++ frontend/src/App.svelte
@@ -0,0 +1,71 @@
+<script  lang="ts">
+  import { onMount } from 'svelte';
+  import { makeServer } from './api'
+  makeServer({ environment: 'development' })
+  import axios from "axios"
+  import z from "zod"
+
+  import LoadingMask from './components/LoadingMask.svelte';
+  import Laptop from './components/Laptop.svelte'
+
+  const LaptopSchema = z.object({
+    brand: z.string(),
+    name: z.string(),
+    weight: z.number()
+  })
+  
+  type Laptop = z.infer<typeof LaptopSchema>
+  
+
+  let searchValue = ""
+  let isLoading = true
+  let isSorted = false
+
+  let laptops: Laptop[] = []
+  let searchedLaptops: Laptop[] = []
+  $: searchedLaptops = searchValue ? laptops.filter( laptop => laptop.name.includes(searchValue)) : laptops
+
+  onMount (async () => {
+    isLoading = true
+    const response = await axios.get("https://demoapi.com/api/laptop")
+    isLoading = false
+
+    /* const responseResult = LaptopSchema.safeParse(response)
+    if (!responseResult.success) {
+      laptops = []
+    }else {
+      laptops = responseResult.data
+    } */
+
+    laptops = response.data
+  })
+  
+  
+  const sort = () => {
+    searchedLaptops = searchedLaptops.sort((a,b) =>{
+      return isSorted ? a.weight - b.weight : b.weight - a.weight
+    })
+    isSorted = !isSorted
+  }
+
+  
+</script>
+
+<title>Laptops</title>
+
+<header class=" flex flex-row justify-center">
+  <input type="text" bind:value={searchValue}>
+  <button class=" btn btn-primary" on:click={sort}>Sort</button>
+</header>
+
+<main class=" mt-20 flex flex-row justify-center ">
+  {#if isLoading}
+    <LoadingMask />
+  {/if}
+  <div class=" flex flex-row gap-10">
+    {#each searchedLaptops as laptop}
+      <Laptop laptop={laptop} />
+    {/each}
+  </div>
+</main>
+
---
+++ frontend/src/components/Laptop.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import z from "zod"
+    
+    const LaptopSchema = z.object({
+    brand: z.string(),
+    name: z.string(),
+    weight: z.number()
+    })
+
+    type Laptop = z.infer<typeof LaptopSchema>
+
+    export let laptop:Laptop
+    let isShowingMore = false
+
+    const showMore = () => {
+        isShowingMore = !isShowingMore
+    }
+
+</script>
+
+<div>
+    <p>
+        <span class=" text-3xl">{laptop.name}</span>
+        <button class=" btn btn-primary btn-xs" on:click={showMore}>
+            {isShowingMore ? "Show less" : "Show more"}
+        </button>
+    </p>
+    {#if isShowingMore}
+        <p class=" text-3xl">{laptop.brand}</p>
+        <p class=" text-3xl">{laptop.weight}</p>
+    {/if}
+
+</div>
